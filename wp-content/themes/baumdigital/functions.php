56852ba317511c87f0a378883b8cbea0e34c350c
Keneth Rosales <kr@smt.gt>
2020-01-25 20:20:55 -0600

    Creacion de nuevo tema inicial

---
+++ wp-content/themes/baumdigital/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+//Define constants
+define('HOMELINK', site_url('/'));
+define('PATH', get_template_directory_uri());
+define('IMAGES', get_template_directory_uri()."/img" );
+define('SITENAME', get_bloginfo('name') );
+
+?>
