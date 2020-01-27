00edf1657ff4185a697d22e3a81c138ed6b7cee3
Keneth Rosales <kr@smt.gt>
2020-01-26 20:08:57 -0600

    Ajustes necesarios para la navegacion de la prueba

--- wp-content/themes/baumdigital/footer.php
+++ wp-content/themes/baumdigital/footer.php
@@ -1,4 +1,8 @@
-
+<footer class="mastfoot mt-auto">
+    <div class="inner">
+      <p class="center">Tema desarrollado por Keneth Rosales con <a href="https://getbootstrap.com/">Bootstrap</a>.</p>
+    </div>
+  </footer>
  <script src="<?php echo PATH?>/js/bootstrap.min.js"></script>
  <script src="<?php echo PATH?>/js/scripts.js"></script>
 <?php wp_footer(); ?>
--- wp-content/themes/baumdigital/home.php
+++ wp-content/themes/baumdigital/home.php
@@ -1,4 +1,17 @@
 <?php get_header(); ?>
-Este es el home
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="padding-top-lg padding-bottom-lg">
+					Prueba Baum Digital
+				</h1>
+				<p>
+					Esta es una prueba para Baum digital desde Guatemala
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
 
 <?php get_footer(); ?>
